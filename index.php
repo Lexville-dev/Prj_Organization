7d49acada7e7ba5a7289806a1cde5bb9ad94e669
Lexville-dev <Bryndavies2015@gmail.com>
2021-11-12 14:33:50 +0000

    Index.php base
    skeleton of index.php

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php include "includes/header.php" ?>
+<div class="container">
+  <h1>Aaaaa</h1>
+    <form>
+
+      <!-- <div class="container">
+        <div class="row">
+          <div class="col-1">1</div>
+          <div class="col-1">2</div>
+          <div class="col-1">3</div>
+          <div class="col-1">4</div>
+          <div class="col-1">5</div>
+          <div class="col-1">6</div>
+          <div class="col-1">7</div>
+          <div class="col-1">8</div>
+          <div class="col-1">9</div>
+          <div class="col-1">10</div>
+          <div class="col-1">11</div>
+          <div class="col-1">12</div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-2">1</div>
+          <div class="col-2">2</div>
+          <div class="col-2">3</div>
+          <div class="col-2">4</div>
+          <div class="col-2">5</div>
+          <div class="col-2">6</div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-3">1</div>
+          <div class="col-3">2</div>
+          <div class="col-3">3</div>
+          <div class="col-3">4</div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-4">1</div>
+          <div class="col-4">2</div>
+          <div class="col-4">3</div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-6">1</div>
+          <div class="col-6">2</div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-12">1</div>
+        </div>
+      </div> -->
+
+        <div class="container">
+          <div class="form-group">
+              <label for="exampleFormControlInput1">Request Title</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
+          </div>
+
+          <div class="row">
+            <div class="col-xs-12 col-sm-6">
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Name</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
+              </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6">
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Contact Number</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="01234 12345">
+              </div>
+            </div>
+        </div>
+
+          <div class="form-group">
+              <label for="exampleFormControlInput1">Email</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="example@example.com">
+            </div>
+
+
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">Subject</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+        </div>
+      </div>
+
+          <div class="form-group">
+            <a href="#" class="btn btn-primary"> Submit </a>
+          </div>
+  </div>
+</form>
+
+<?php include "includes/footer.php" ?>
